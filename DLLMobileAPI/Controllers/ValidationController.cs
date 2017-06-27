4182f9d5c834d19c53a1d078f6266e381a5871d4
Lucas Felix <lucas@jdev.com.br>
2017-06-27 18:58:06 -0300

    changes

---
+++ DLLMobileAPI/Controllers/ValidationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace DLLMobileAPI.Controllers
+{
+    [RoutePrefix("validations")]
+    public class ValidationController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IHttpActionResult> ExistsCpf([FromUri]long cpf)
+        {
+            try
+            {
+                using (var context = new ApiContext())
+                {
+                    return Ok(new { exists = context.Users.Any(u => u.Cpf == cpf) });
+                }
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
