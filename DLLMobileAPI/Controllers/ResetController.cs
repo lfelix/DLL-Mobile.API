50d176096c38a7a0271d8e1ab8d18a44982d6630
Lucas Felix <lucas@jdev.com.br>
2017-07-19 10:17:00 -0300

    no escurinho

--- DLLMobileAPI/Controllers/ResetController.cs
+++ DLLMobileAPI/Controllers/ResetController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using crypt = BCrypt.Net;
 
 namespace DLLMobileAPI.Controllers
 {
@@ -31,6 +33,44 @@ namespace DLLMobileAPI.Controllers
 
             return Ok(new { username = username });
         }
+
+
+        [HttpPut]
+        [AllowAnonymous]
+        public async Task<IHttpActionResult> ChangePassword(RecoverUserCommand recoverUserCommand)
+        {
+            string message;
+
+            try
+            {
+                using (var context = new ApiContext())
+                {
+                    var userEdited = context.Users.FirstOrDefault(u => u.Cpf == recoverUserCommand.Cpf);
+                    if (userEdited != null)
+                    {
+                        var salt = crypt.BCrypt.GenerateSalt();
+                        string encryptedPassword = crypt.BCrypt.HashPassword(recoverUserCommand.NewPassword, salt);
+
+                        userEdited.Password = encryptedPassword;
+
+                        context.Users.AddOrUpdate(userEdited);
+                        await context.SaveChangesAsync();
+                        message = "Usuário alterado com sucesso.";
+
+                        return Ok(new { success = true, message = message });
+                    }
+                    else
+                    {
+                        return BadRequest("Usuário não encontrado.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Ocorreu um erro ao alterar usuário.");
+            }
+        }
+
 
         [AllowAnonymous]
         [HttpPost]
