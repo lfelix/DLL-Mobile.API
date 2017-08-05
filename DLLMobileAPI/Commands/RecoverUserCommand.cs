9e5b888fc5062ef5b0067958c456a06e9b607d49
Lucas Felix <lucas@jdev.com.br>
2017-08-05 14:36:05 -0300

    revert crediare 12

---
+++ DLLMobileAPI/Commands/RecoverUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DLLMobileAPI.Controllers
+{
+    public class RecoverUserCommand
+    {
+        public long Cpf { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
